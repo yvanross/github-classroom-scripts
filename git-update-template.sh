c0ce8bb6786dda13170dfd094d9194f9f3ce1adf
Yvan Ross <Yvan.ross@gmail.com>
2021-01-01 12:00:59 -0500

    ask student identification

---
+++ git-update-template.sh
@@ -0,0 +1,9 @@
+echo "Must implement and test, see comment in this file"
+' Objective is to update student code when template was modified.
+' https://stackoverflow.com/questions/56577184/github-pull-changes-from-a-template-repository
+' git remote add template [URL of the template repo] --allow-unrelated-histories
+' --allow-unrelated-histories only if error is generated
+' git fetch --all
+' git merge template/[branch to merge
+' the process should include a task to do manual merge when requested
+
