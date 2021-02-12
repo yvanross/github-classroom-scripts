b61459a0b13737870329934c900243948940f614
Yvan Ross <Yvan.ross@gmail.com>
2021-02-11 20:10:37 -0500

    update some scripts

--- git-push.sh
+++ git-push.sh
@@ -2,19 +2,19 @@
 
 if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
   echo "third parameter is the find maxdepth for directory"
-  echo './git-push.sh "/Users/rossypro/Downloads/LOG210-LAB0" "message" 2' 
-  echo './git-push.sh "/Users/rossypro/Downloads/LOG210-LAB1" "message" 1'
+  echo './git-push.sh "/Users/rossypro/Downloads/LOG210-LAB0" 2 "message" ' 
+  echo './git-push.sh "/Users/rossypro/Downloads/LOG210-LAB1" 1 "message" '
 else 
   clear
   counter=0
-  for group in $(find $1 -maxdepth $3 -type d); do
+  for group in $(find $1 -maxdepth $2 -type d); do
     counter=$(( $counter + 1 ))
     cd "$group"
     echo "------------------------------------------------------------------- "
     echo "$counter => $group"
-    git pull
+    git pull origin master
     git add .
-    git commit -am "$2"
+    git commit -am "$3"
     git push --set-upstream origin master
   done
 fi
