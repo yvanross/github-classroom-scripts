7a39ac6dc7284f8c56d272dd411d9286413964a4
Yvan Ross <Yvan.ross@gmail.com>
2021-02-27 08:50:43 -0500

    clean up

--- git-enable.sh
+++ git-enable.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
+# enlève le token dans le git remote -v provenant de Github classroom.  Cela permet par la suite de faire des git push et pull sans problèmes.
 # A POSIX variable
   echo -e "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 
 if [ -z "$1" ]; then
-  echo './git-enable.sh  "/Users/rossypro/Downloads/S20203-lab1-rapport"'
+  echo './git-enable.sh  "/Users/rossypro/Downloads/S20203-lab1-rapport" 2'
   exit 0
 fi
 clear
 for group in $(find $1 -maxdepth $2 -type d); do
      cd "$group"
-     
+ 
     echo "$group"
     origin=$(git remote get-url --push origin)
     if [[ $origin == *"@github.com:cc-yvan"* ]]; then
       echo "already modified"
     else
+      echo "$origin"
       new_origin="${origin#*/}"
       new_origin=$(echo "git@github.com:$new_origin")
+      echo "$new_origin"
+      echo "    "
       git remote remove origin
       git remote add origin $new_origin
     fi
